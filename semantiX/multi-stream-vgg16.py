f247484fccfca0d088bfe7cb30a67b5a8c46a998
gpspelle <gpsunicamp016@gmail.com>
2018-08-01 19:35:26 -0300

    script to rename weights and new archt

--- semantiX/multi-stream-vgg16.py
+++ semantiX/multi-stream-vgg16.py
@@ -19,128 +19,9 @@ from keras.applications.vgg16 import preprocess_input
     Semantix. 
 '''
 
-''' Documentation: class VGG16 
+''' Documentation: script multi-stream-vgg16 
     
-    This class has only one method:
-
-    weight_init
-
-    The methods that should be called outside of this class are:
-
-    weight_init: receives as parameter a file containing weights of a trained
-    CNN
 '''
-
-class VGG16_temporal: 
-
-    def __init__(self, num_features, x_size, y_size):
-
-        self.model = None
-        self.layers_name = []
-        self.num_features = num_features
-        self.x_size = x_size
-        self.y_size = y_size
-        
-        self.sliding_height = 10
-
-        self.layers_name = ['conv1_1', 'conv1_2', 'conv2_1', 'conv2_2', 
-                'conv3_1', 'conv3_2', 'conv3_3', 'conv4_1', 'conv4_2', 
-                'conv4_3', 'conv5_1', 'conv5_2', 'conv5_3', 'fc6', 'fc7', 
-                'fc8']
-        self.model = Sequential()
-
-        self.model.add(ZeroPadding2D((1, 1), input_shape=(self.x_size, 
-                       self.y_size, 2*self.sliding_height)))
-        self.model.add(Convolution2D(64, (3, 3), activation='relu', 
-                       name='conv1_1'))
-        self.model.add(ZeroPadding2D((1, 1)))
-        self.model.add(Convolution2D(64, (3, 3), activation='relu', 
-                       name='conv1_2'))
-        self.model.add(MaxPooling2D((2, 2), strides=(2, 2)))
-
-        self.model.add(ZeroPadding2D((1, 1)))
-        self.model.add(Convolution2D(128, (3, 3), activation='relu',
-                  name='conv2_1'))
-        self.model.add(ZeroPadding2D((1, 1)))
-        self.model.add(Convolution2D(128, (3, 3), activation='relu', 
-                  name='conv2_2'))
-        self.model.add(MaxPooling2D((2, 2), strides=(2, 2)))
-
-        self.model.add(ZeroPadding2D((1, 1)))
-        self.model.add(Convolution2D(256, (3, 3), activation='relu',
-                  name='conv3_1'))
-        self.model.add(ZeroPadding2D((1, 1)))
-        self.model.add(Convolution2D(256, (3, 3), activation='relu',
-                  name='conv3_2'))
-        self.model.add(ZeroPadding2D((1, 1)))
-        self.model.add(Convolution2D(256, (3, 3), activation='relu', 
-                  name='conv3_3'))
-        self.model.add(MaxPooling2D((2, 2), strides=(2, 2)))
-
-        self.model.add(ZeroPadding2D((1, 1)))
-        self.model.add(Convolution2D(512, (3, 3), activation='relu', 
-                  name='conv4_1'))
-        self.model.add(ZeroPadding2D((1, 1)))
-        self.model.add(Convolution2D(512, (3, 3), activation='relu', 
-                  name='conv4_2'))
-        self.model.add(ZeroPadding2D((1, 1)))
-        self.model.add(Convolution2D(512, (3, 3), activation='relu', 
-                  name='conv4_3'))
-        self.model.add(MaxPooling2D((2, 2), strides=(2, 2)))
-
-        self.model.add(ZeroPadding2D((1, 1)))
-        self.model.add(Convolution2D(512, (3, 3), activation='relu', 
-                  name='conv5_1'))
-        self.model.add(ZeroPadding2D((1, 1)))
-        self.model.add(Convolution2D(512, (3, 3), activation='relu', 
-                  name='conv5_2'))
-        self.model.add(ZeroPadding2D((1, 1)))
-        self.model.add(Convolution2D(512, (3, 3), activation='relu', 
-                  name='conv5_3'))
-        self.model.add(MaxPooling2D((2, 2), strides=(2, 2)))
-
-        self.model.add(Flatten())
-        self.model.add(Dense(self.num_features, name='fc6', 
-            kernel_initializer='glorot_uniform'))
-
-    def weight_init(self, weights_file):
-
-        '''
-            todo: test if 
-            self.model.load_weights(weights_file)
-            substitui essa funcao toda
-        '''
-
-        '''
-        Input:
-        * weights_file: path to a hdf5 file containing weights and biases to a
-        trained network
-        
-        Output: TODO
-        '''
-
-        h5 = h5py.File(weights_file)
-        
-        layer_dict = dict([(layer.name, layer) 
-                            for layer in self.model.layers])
-
-        # Copy the weights stored in the 'weights_file' file to the feature 
-        # extractor part of the CNN architecture
-        for layer in self.layers_name[:-3]:
-            w2, b2 = h5['data'][layer]['0'], h5['data'][layer]['1']
-            w2 = np.transpose(np.asarray(w2), (3,2,1,0))
-            w2 = w2[::-1, ::-1, :, :]
-            b2 = np.asarray(b2)
-            K.set_value(layer_dict[layer].kernel, w2)
-            K.set_value(layer_dict[layer].bias, b2)
-          
-        # Copy the weights of the first fully-connected layer (fc6)
-        layer = self.layers_name[-3]
-        w2, b2 = h5['data'][layer]['0'], h5['data'][layer]['1']
-        w2 = np.transpose(np.asarray(w2), (1,0))
-        b2 = np.asarray(b2)
-        K.set_value(layer_dict[layer].kernel, w2)
-        K.set_value(layer_dict[layer].bias, b2)
 
 if __name__ == '__main__':
     print("***********************************************************",
@@ -166,63 +47,46 @@ if __name__ == '__main__':
         argp.print_help(sys.stderr)
         exit(1)
     
-
+    sliding_height = 10
+    
     if 'temporal' in args.streams:
-        arch = VGG16_temporal(args.num_features[0], args.input_dim[0], 
-                     args.input_dim[1])
-        
-        arch.weight_init(args.weight[0])
-
-        print("Saving your temporal CNN as VGG16_temporal")
-        arch.model.save('VGG16_temporal')
-
-
-    if 'pose' in args.streams:
-        model = VGG16(weights='imagenet', include_top=False, 
-                      input_shape=(args.input_dim[0], args.input_dim[1], 3))
+        model = VGG16(include_top=False, input_shape=(args.input_dim[0], 
+                        args.input_dim[1], 2*sliding_height))
 
         top_model = Sequential()
         top_model.add(Flatten(input_shape=model.output_shape[1:]))
         top_model.add(Dense(args.num_features[0], name='fc6', 
                   kernel_initializer='glorot_uniform'))
 
-        h5 = h5py.File(args.weight[0])
-        layer_dict = dict([(layer.name, layer) 
-                            for layer in top_model.layers])
-        # Copy the weights of the first fully-connected layer (fc6)
-        layer = 'fc6'
-        w2, b2 = h5['data'][layer]['0'], h5['data'][layer]['1']
-        w2 = np.transpose(np.asarray(w2), (1,0))
-        b2 = np.asarray(b2)
-        K.set_value(layer_dict[layer].kernel, w2)
-        K.set_value(layer_dict[layer].bias, b2)
+        model.load_weights(args.weight[0], by_name=True)
+        model = Model(inputs=model.input, outputs=top_model(model.output))
+        print("Saving your temporal CNN as VGG16_temporal")
+        top_model.save('VGG16_temporal')
 
+    if 'pose' in args.streams:
+        model = VGG16(include_top=False, 
+                        input_shape=(args.input_dim[0], args.input_dim[1], 3))
 
+        top_model = Sequential()
+        top_model.add(Flatten(input_shape=model.output_shape[1:]))
+        top_model.add(Dense(args.num_features[0], name='fc6', 
+                  kernel_initializer='glorot_uniform'))
+
+        model.load_weights(args.weight[0], by_name=True)
         model = Model(inputs=model.input, outputs=top_model(model.output))
         print("Saving your pose-estimation CNN as VGG16_pose")
         model.save('VGG16_pose')
     
     if 'spatial' in args.streams:
-        model = VGG16(weights='imagenet', include_top=False, 
-                      input_shape=(args.input_dim[0], args.input_dim[1], 3))
+        model = VGG16(include_top=False, 
+                        input_shape=(args.input_dim[0], args.input_dim[1], 3))
 
         top_model = Sequential()
         top_model.add(Flatten(input_shape=model.output_shape[1:]))
         top_model.add(Dense(args.num_features[0], name='fc6', 
                   kernel_initializer='glorot_uniform'))
 
-        h5 = h5py.File(args.weight[0])
-        layer_dict = dict([(layer.name, layer) 
-                            for layer in top_model.layers])
-        # Copy the weights of the first fully-connected layer (fc6)
-        layer = 'fc6'
-        w2, b2 = h5['data'][layer]['0'], h5['data'][layer]['1']
-        w2 = np.transpose(np.asarray(w2), (1,0))
-        b2 = np.asarray(b2)
-        K.set_value(layer_dict[layer].kernel, w2)
-        K.set_value(layer_dict[layer].bias, b2)
-
-
+        model.load_weights(args.weight[0], by_name=True)
         model = Model(inputs=model.input, outputs=top_model(model.output))
         print("Saving your spatial CNN as VGG16_spatial")
         model.save('VGG16_spatial')
