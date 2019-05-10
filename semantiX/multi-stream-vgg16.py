1a97fe80b5a54efbf4da80a49d09f6448dac0dd5
gpspelle <gpsunicamp016@gmail.com>
2019-05-09 21:26:00 -0300

    [no changes] commenting multi-stream-vgg16.py

--- semantiX/multi-stream-vgg16.py
+++ semantiX/multi-stream-vgg16.py
@@ -17,12 +17,60 @@ from keras.applications.vgg16 import VGG16
 
 ''' Documentation: script multi-stream-vgg16 
 
+
+    FOR USING: in order to use it the recommended procedure is to call the
+    desired .py without parameters, for example:
+
+    $ multi-stream-vgg16.py
+
+    Check the necessary parameters and theirs usage informations. Fill the
+    parameters as you need them.
+
     This code use Kera's applications in deep learning to create and load
     weights for the possibilities of CNN's that are implemented here.
     
     Temporal: input are a 10+10 stack of optical flows (mag, ord)
     Pose: imput are RGB drawed skeletons using tf-pose-estimation
     Spatial: input are RGB images
+    Pose: input are pose skeletons images
+    Depth: input are depth images
+    Ritmo: input is a transformation of a video into an image
+
+    Some important values are harcoded: number of outputs of VGG16 
+    (num_features), the size of the sliding window for stack streams
+    (sliding window), the x and y dimension of the input video.
+
+    There are two kinds of VGG16 being created with this code, those which use
+    a stack approach and others using RGB approach. 
+
+    Stack approach uses weights_vgg16_STACK.h5 input, that is an pre-trained
+    VGG16 in imagenet + UCF101. UCF101 is a human action dataset.
+    
+    RGB approach uses weights_vgg16_RGB.h5 input, that is an pre-trained
+    VGG16 in imagenet.
+
+    FUTURE DEVELOPMENT: in order to add a new stream, there are a few examples
+    of cases.
+
+    First of all you need to identify the shape of your new input stream.
+    If your stream has one data sample for every frame of your video, then it's
+    a RGB stream. But, if your stream has one data sample for a collection of
+    sliding_height frames, it is, you need a stack of size sliding_height to
+    have an input information, then you have a new STACK stream.
+
+    An example of each: spatial stream use raw RGB data from the video. Whereas, 
+    temporal stream stacks the content of 10 frames to create an input
+    information. 
+    
+    If you have a nice abstraction of this ocurring in a finite
+    input video, you can see that the last (sliding_heights - 1) frames can't 
+    make a stack. Besides, note that temporal is not using raw frames to stack,
+    it's using optical flow, which is using 2 frames to create one optical flow.
+    Therefore, the last frame can't make an optical flow for it. And that makes 
+    temporal to cut the last sliding_height frames. But That's a discussion that
+    matters only for the next steps, in specific,  
+    
+    including streams_fextractor.py.
 
 '''
 
@@ -47,28 +95,23 @@ if __name__ == '__main__':
         argp.print_help(sys.stderr)
         exit(1)
     
-    num_features = 4096
-    sliding_height = 10
-    x_dim = 224
-    y_dim = 224
+    num_features = 4096     # Number of outputs of our CNN
+    sliding_height = 10     # Number of elements inside a window
+    x_dim = 224             # X input size
+    y_dim = 224             # Y input size
 
     weights = dict()
 
+    # Filling a dict with corresponding values of transfer learning files
     for w in args.weight:
         if 'RGB' in w:
             weights['RGB'] = w
         else:
             weights['STACK'] = w
-    
+
+    # Check the existence of every possible stream in input argument
     if 'temporal' in args.streams:
         
-        # All this must be done instead of calling VGG16 as the others, because
-        # of the input format needs a channel 3, and for temporal we're using
-        # 2 * sliding_height as channel. 
-
-
-        # TODO: CAN BE CHANGED, SEE multi-stream-resnet50.py
-
         stack_input = (x_dim, y_dim, 2*sliding_height)
         tensor_stack_input = Input(shape=stack_input)
 
@@ -307,5 +350,3 @@ if __name__ == '__main__':
 
         print("Saving your spatial CNN as VGG_ritmo")
         model.save('VGG16_ritmo')
-
-
